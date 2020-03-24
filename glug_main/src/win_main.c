e2741637281194d9623d879528dfecb41aa9efe4
Dustin Cook <dustin@libglug.org>
2020-03-24 13:09:58 -0700

    update build scripts, submodules

--- glug_main/src/win_main.c
+++ glug_main/src/win_main.c
@@ -3,10 +3,10 @@
 
 int WINAPI WinMain(HINSTANCE inst, HINSTANCE prev, LPSTR cmd, int show_flag)
 {
-  (void)inst;
-  (void)prev;
-  (void)cmd;
-  (void)show_flag;
+    (void)inst;
+    (void)prev;
+    (void)cmd;
+    (void)show_flag;
 
-  return glug_main(__argc, __argv);
+    return glug_main(__argc, __argv);
 }
