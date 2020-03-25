3986aafccd862c38bd09d0193df654159d9ac430
Dustin Cook <dustin@libglug.org>
2020-03-25 12:04:34 -0700

    Appveyor: target release build

--- test/main.c
+++ test/main.c
@@ -13,7 +13,7 @@ int glug_main(int argc, char *argv[])
     printf("assert argc == 3 ([0] binary name, [1,2] arguments)\n");
     assert(argc == 3);
 
-   printf("assert argv[1] == HELLO\n");
+    printf("assert argv[1] == HELLO\n");
     assert(!strcmp(argv[1],  "HELLO"));
 
     printf("assert argv[2] == glug\n");
